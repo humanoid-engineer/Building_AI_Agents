af499bbedaa5aa4a11e99d2e9590412a7d7d153c
humanoid-engineer <mathivanancloud@gmail.com>
2025-11-29 17:22:33 +0530

    React and Anti Halluncination Examplese Updated

--- src/17_Tool_and_Anti-Hallucination.py
+++ src/17_Tool_and_Anti-Hallucination.py
@@ -6,13 +6,21 @@ if not api_key:
 	raise RuntimeError("OPENAI_API_KEY environment variable is not set")
 client = OpenAI(api_key=api_key)
 
+goal = input("Enter the goal for the agent: ")
+
 prompt = """
-You are an agent. Use only the tool: search(query).
-Goal: Find EV sales growth in India.
+You are an agent. You have to find which tool to use to achive the goal. 
+Tools are search(query), calculator(query).
+
 FORMAT:
 Thought:
-Action: search["..."]
+Action: search["..."] or calculator["..."]
+
+If you unable to find the tool, respond "Tool Not Found".
+
 """
+prompt += f"Goal: {goal}\n"
+
 resp = client.chat.completions.create(
 model="gpt-4.1-mini",
 messages=[{"role": "user", "content": prompt}]
